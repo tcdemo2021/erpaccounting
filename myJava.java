6b5dbd276aafff614c3f76ec89a997b7cd1df8d7
tcdemo2021 <85601377+tcdemo2021@users.noreply.github.com>
2021-06-09 10:01:07 +0530

    Add files via upload

---
+++ myJava.java
@@ -0,0 +1,20 @@
+public class myJava {
+
+   public static void main(String args[]){
+
+      	int a = 500;
+		int b = 100;
+		
+		if (a<b) {
+			
+			System.out.println("B is the largest number " + b);
+			
+		}
+		else {
+			
+			System.out.println("A is the largest number " + a);
+		}
+
+   }
+
+}
